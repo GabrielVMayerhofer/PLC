92b6650e4a7c0f5d076d2c434d146b04801961ec
gvm <gvm@g3c37.cin.ufpe.br>
2025-06-02 09:40:16 -0300

    exercicio Aula 5

--- Aulas/Aula5.hs
+++ Aulas/Aula5.hs
@@ -14,3 +14,13 @@ iter 0 f = id
 iter n f = (iter (n-1) f).f
 
 -- x + y = (\x -> \y -> x + y)
+
+-- exercicio
+primo :: Int -> Bool
+primo n = dividindo (n-1)
+  where dividindo 1 = True
+        dividindo x | mod n x /= 0 = dividindo (x-1)
+                    | otherwise = False
+
+sumPrimeSqr :: Int -> Int -> Int
+sumPrimeSqr a b = foldl1 (+) (map (^2) (filter primo [a..b]))
