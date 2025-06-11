924f3cace4b9d68b4841c320b338c3c8154e0ba2
GabrielVMayerhofer <gabrielmayerhofer@gmail.com>
2025-06-11 20:33:43 -0300

    Lista 2 - Completa

--- Listas/Lista2/QG_DNA2.hs
+++ Listas/Lista2/QG_DNA2.hs
@@ -1,29 +1,40 @@
 data Animal = Cisnal | Iguanoide | Narvale | Null
   deriving (Eq, Show)
 
-trasformar :: [Animal] -> [Int]
-transformar [] = []
-transformar xs = foldr (\x accC accI accN -> case xs of Cisnal -> accC + 1
-                                                        Iguanoide -> accI + 1
-                                                        Narvale -> accN + 1
-                                                        _ -> ) (0,0,0) xs
+transformar :: [Animal] -> [Int]
+transformar [] = [0,0,0]
+transformar xs = [c,i,n]
+  where (c,i,n) = foldl (\(accC,accI,accN) x -> case x of 
+                    Cisnal -> (accC+1, accI, accN)
+                    Iguanoide -> (accC, accI+1, accN)
+                    Narvale -> (accC, accI, accN+1)
+                    _ -> (accC, accI, accN)
+                  ) (0,0,0) xs
 
-contar :: Float -> [Animal]
-contar indicador = case indicador of
-  indicador >= 0.1 && indicador <= 0.3 -> [Cisnal] 
-  indicador >= 0.4 && indicador <= 0.7 -> [Iguanoide] 
-  indicador >= 0.8 -> [Narvale] 
-  otherwise -> [Null] 
+contar :: [Float] -> [Animal]
+contar [] = []
+contar (indicador : xs)
+  | indicador >= 0.1 && indicador <= 0.3 = Cisnal : contar xs 
+  | indicador >= 0.4 && indicador <= 0.7 = Iguanoide : contar xs
+  | indicador >= 0.8 = Narvale : contar xs
+  | otherwise = Null : contar xs
 
-indice :: String -> String -> Float
-indice [] _ = 0
-indice _ [] = 0
-indice firstE secondE = div (foldl (\acc (indicador,y) -> if indicador == y then acc + 1 else acc) 0 (zip firstE secondE)) (max (length firstE) (length secondE))
+contadorStr :: String -> String -> Int
+contadorStr _ [] = 0
+contadorStr [] _ = 0
+contadorStr (x:xs) (y:ys)
+  | x == y = 1 + contadorStr xs ys
+  | otherwise = contadorStr xs ys
+
+indice :: [String] -> [String] -> [Float]
+indice [] _ = []
+indice _ [] = []
+indice firstE secondE = [fromIntegral (contadorStr a b) / fromIntegral (max (length a) (length b)) | (a,b) <- zip firstE secondE]
 
 dna2 :: [String] -> [String] -> [Int]
-dna2 _ [] = []
-dna2 [] _ = []
-dna2 xs ys = 
+dna2 str1 str2 = transformar (contar (indice str1 str2))
+
+--compara -> enlista animais -> conta
 
 main = do
   firstExtract <- words <$> getLine                       -- equivalente a (read firstExtract :: [String])
