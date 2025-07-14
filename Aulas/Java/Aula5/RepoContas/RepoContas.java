7220bd9fff384b446764fb718f7a69004edb1d23
Gabriel Valenca Mayerhofer <gvm@WINDOWS.CIN.UFPE.BR>
2025-07-14 09:48:01 -0300

    TO-DO: RepoContas

---
+++ Aulas/Java/Aula5/RepoContas/RepoContas.java
@@ -0,0 +1,7 @@
+public interface RepoContas{
+  void inserir(ContaAbstrata conta);
+  ContaAbstrata procurar(String numero);
+  void remover(String numero);
+  void atualizar(ContaAbstrata conta);
+  boolean existe(String numero);
+}
