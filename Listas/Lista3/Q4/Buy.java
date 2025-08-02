4500e5bda2eb46d3c1efcfceb89795a7bebdecc8
GabrielVMayerhofer <gabrielmayerhofer@gmail.com>
2025-08-02 17:12:17 -0300

    Q4 concluida, Q2 em andamento

---
+++ Listas/Lista3/Q4/Buy.java
@@ -0,0 +1,36 @@
+public class Buy {
+		private Cart chart;
+
+		public Buy() {
+				chart = new Cart();
+		}
+
+		public void addItemToChart(Items item) { //Add item ao carrinho
+				try {
+						chart.addItem(item);
+				} catch (NotEnoughSpace | ItemAlreadyExists | NullParameters e) {
+						System.out.println("Erro ao adicionar item: " + e.getMessage());
+				}
+		}
+
+		public void removeItemFromChart(String name) { //Remove item pelo nome
+				try {
+						chart.removeItem(name);
+				} catch (NotFound | NullParameters e) {
+						System.out.println("Erro ao remover item: " + e.getMessage());
+				}
+		}
+
+		public void searchItemInChart(String name) { //Pesquisa item pelo nome
+				try {
+						String result = chart.searchItem(name);
+						System.out.println(result);
+				} catch (NotFound | NullParameters e) {
+						System.out.println("Erro ao buscar item: " + e.getMessage());
+				}
+		}
+
+		public void displayChartItems() { //Print de todos itens no carrinho
+				chart.getItems();
+		}
+}
